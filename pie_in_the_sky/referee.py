1a5603a8226c15be5223bc5a57b7e39adf6ea28c
Kale Kundert <kale@thekunderts.net>
2015-12-06 19:51:36 -0800

    Send StartGame after the players have been created.
    This required a change to the game engine, otherwise the referee
    couldn't know how many players to expect.

--- pie_in_the_sky/referee.py
+++ pie_in_the_sky/referee.py
@@ -5,8 +5,22 @@ from . import messages
 
 class Referee (kxg.Referee):
 
-    def on_start_game(self):
-        from vecrec import Vector
-        position = Vector(100, 100)
-        velocity = Vector.null()
-        self >> messages.StartGame(position, velocity)
+    def __init__(self):
+        super().__init__()
+        self.num_players_expected = None
+
+    def on_start_game(self, num_players):
+        self.num_players_expected = num_players
+
+    @kxg.subscribe_to_message(messages.CreatePlayer)
+    def on_create_player(self, message):
+        num_players_joined = len(self.world.players)
+        kxg.info("{num_players_joined} of {self.num_players_expected} players created.")
+
+        if num_players_joined == self.num_players_expected:
+            from vecrec import Vector
+            position = Vector(100, 100)
+            velocity = Vector.null()
+            self >> messages.StartGame(position, velocity)
+
+
