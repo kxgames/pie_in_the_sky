874396ffd7b9edd8666a73ac0f280f1abba81572
Kale Kundert <kale@thekunderts.net>
2016-01-16 18:30:44 -0800

    Make targets bounce off each other.

--- pie_in_the_sky/tokens.py
+++ pie_in_the_sky/tokens.py
@@ -34,27 +34,66 @@ class FieldObject(kxg.Token):
     A base class for targets, bullets, obstacles, and other objects that are in the field of play. Instances of this base class can be used if special methods are not necessary for a type of object. This class defines default functionality for motion, collisions, and updating.
     """
 
-    def __init__(self, position, velocity, mass=1, collision_distance=20):
+    def __init__(self, position, velocity, mass=1, radius=20):
         super().__init__()
 
         self.mass = float(mass)
         self.position = position
         self.velocity = velocity
         self.acceleration = Vector.null()
-        self.collision_distance = float(collision_distance)
-        self.collision_distance_squared = collision_distance**2
+        self.radius = float(radius)
+        self.vulnerable_to_bullets = True
 
         self.next_position = Vector.null()
         self.next_velocity = Vector.null()
         self.next_acceleration = Vector.null()
 
-    def can_collide_with(self, object):
-        distance_squared = self.calculate_distance_squared(object)
-        return distance_squared <= self.collision_distance_squared
+    def move(self):
+        self.position = self.next_position
+        self.velocity = self.next_velocity
+        self.acceleration = self.next_acceleration
 
-    def calculate_distance_squared(self, object):
-        delta = self.position - object.position
-        return delta.magnitude_squared
+        self.next_position = Vector.null()
+        self.next_velocity = Vector.null()
+        self.next_acceleration = Vector.null()
+
+    def bounce(self, other):
+        # Define the x-axis to coincide with the line between the centers of 
+        # the two colliding objects and the y-axis to be orthogonal to the 
+        # x-axis.  The x component of the velocities will bounce as if this 
+        # were a 1D problem, and the y components will not change.
+
+        offset = other.position - self.position
+        distance = offset.magnitude
+
+        x = offset.unit
+        y = x.orthogonal
+
+        v1x = self.velocity.dot(x)
+        v1y = self.velocity.dot(y)
+        v2x = other.velocity.dot(x)
+        v2y = other.velocity.dot(y)
+
+        # Calculate the new velocities using the equations for a 1D elastic 
+        # collision.
+
+        m1, m2 = self.mass, other.mass
+
+        v1x_final = v1x * (m1 - m2) / (m2 + m1) + v2x * (2 * m2) / (m2 + m1)
+        v2x_final = v1x * (2 * m1) / (m1 + m2) + v2x * (m2 - m1) / (m1 + m2)
+
+        # Set the final velocities by summing the x and y components of the 
+        # velocity.
+
+        self.velocity = y * v1y +  x * v1x_final
+        other.velocity = y * v2y + x * v2x_final
+
+    def is_touching(self, other):
+        offset = self.position - other.position
+        return offset.magnitude < self.radius + other.radius
+
+    def on_hit_by_bullet(self, bullet):
+        pass
 
     def add_next_acceleration(self, acceleration):
         self.next_acceleration += acceleration
@@ -70,20 +109,11 @@ class FieldObject(kxg.Token):
         self.next_position = p + dp
         self.next_velocity = v + dv
 
-    def move(self):
-        self.position = self.next_position
-        self.velocity = self.next_velocity
-        self.acceleration = self.next_acceleration
-        
-        self.next_position = Vector.null()
-        self.next_velocity = Vector.null()
-        self.next_acceleration = Vector.null()
-
 
 class Bullet(FieldObject):
 
     def __init__(self, cannon, position, velocity):
-        super().__init__(position, velocity)
+        super().__init__(position, velocity, mass=1, radius=5)
         self.cannon = cannon
         
     def __extend__(self):
@@ -98,7 +128,7 @@ class Bullet(FieldObject):
 class Target(FieldObject):
 
     def __init__(self, position, velocity):
-        super().__init__(position, velocity)
+        super().__init__(position, velocity, mass=10, radius=15)
         
     def __extend__(self):
         from . import gui
